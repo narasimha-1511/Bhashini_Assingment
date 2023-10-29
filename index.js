6f951f81081f0f21f74afaf7eb54faa582ace3fd
narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>
2023-10-29 18:04:49 +0530

    Fixing Still
    Signed-off-by: narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require("path");
 
 // when '/' is called it must hit th public folder
-app.use(express.static('public'))
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
 app.use(express.json());
 app.use('/scaler/translate',require("./routes/route.js"));
 
